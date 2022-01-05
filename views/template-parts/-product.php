136cbd4b9de54898dcc8dba7a20eae662d55bc4e
Aslam Doctor <aslam.doctor@gmail.com>
2022-01-05 19:17:29 +0530

    do not escape post title

--- views/template-parts/-product.php
+++ views/template-parts/-product.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="rwpp-product" data-id="<?php echo esc_attr( $post->ID ); ?>">
 
 	<div class="rwpp-product-main">
-		<span class="rwpp-product-title"><?php echo esc_html( $serial_no ); ?>. <?php echo esc_html( $post->post_title ); ?></span>
+		<span class="rwpp-product-title"><?php echo esc_html( $serial_no ); ?>. <?php the_title(); ?></span>
 		<span class="rwpp-product-movement">
 			<span class="move-top dashicons dashicons-arrow-up-alt" title="Move to top of list"></span>
 			<span class="move-bottom dashicons dashicons-arrow-down-alt" title="Move to bottom of list"></span>
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<div class="rwpp-product-info">
 		<?php echo $product->get_image(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 		<div class="rwpp-product-details">
-			<div class="rwpp-product-name"><strong><?php echo esc_html( $post->post_title ); ?></strong> (ID: <?php echo esc_html( $post->ID ); ?>)</div>
+			<div class="rwpp-product-name"><strong><?php the_title(); ?></strong> (ID: <?php echo esc_html( $post->ID ); ?>)</div>
 			<div class="rwpp-product-sku">SKU: <?php echo $product->get_sku(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></div>
 			<div class="rwpp-product-price"><?php echo $product->get_price_html(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></div>
 			<a href="<?php the_permalink(); ?>" class="button" target="_blank">View Product</a>
